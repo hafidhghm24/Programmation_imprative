937ea731316fe0eee59da511fc47dcc20b261654
GHOUILEM Abdelhafidh <111004606+hafidhghm24@users.noreply.github.com>
2025-11-13 08:47:12 +0100

    Add files via upload

---
+++ TPC/TP_Pile_Statique_P1/element.h
@@ -0,0 +1,16 @@
+#ifndef TP_PILE_STATIQUE_P1
+#define TP_PILE_STATIQUE_P1
+
+	/* definir le type element */
+	typedef int ELEMENT;
+	
+	/* prototype des fonctions */
+	
+	void affiche_ELEMENT(ELEMENT element);
+	void saisir_ELEMENT(ELEMENT* ptr_elm);
+	void compare_ELEMENT(ELEMENT* ptr_elm1, ELEMENT* ptr_elm2);
+	void affect_ELEMENT(ELEMENT ptr_elm1, ELEMENT* ptr_elm2);
+
+
+
+#endif
